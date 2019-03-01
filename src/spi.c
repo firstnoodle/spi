4aa90b44edd621460cf94bdb2ab140746d4afc69
sebastian thielke <seb@redboatopera.com>
2019-03-01 10:16:55 +0100

    cleanup

--- src/spi.c
+++ src/spi.c
@@ -1,10 +1,9 @@
+// Heavily based on (copied from)
+// https://raspberry-projects.com/pi/programming-in-c/spi/using-the-spi-interface
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-
-// these are not used anymore...
-#include <string>
-#include <cstring>
 
 // Needed for SPI port
 #include <fcntl.h>
@@ -12,32 +11,14 @@
 #include <linux/spi/spidev.h>
 #include <unistd.h>
 
+
 int spi_cs0_fd;				//file descriptor for the SPI device
 int spi_cs1_fd;				//file descriptor for the SPI device
 unsigned char spi_mode;
 unsigned char spi_bitsPerWord;
 unsigned int spi_speed;
 
-#define DISPLAY_OFF                 0xae
-#define SET_MULTIPLEX_RATIO         0xa8 // 0x3f => 63d = 64 MUX
-#define SET_DISPLAY_OFFSET          0xd3 // 0x00 => no offset
-#define SET_DISPLAY_START_LINE      0x40
-#define SET_SEGMENT_REMAP_start     0xa0 // start/end are not correct descriptions...
-#define SET_SEGMENT_REMAP_end       0xa1
-#define SET_COM_OUTPUT              0xc0
-#define SET_COM_OUTPUT_SCAN_DIR     0xc8
-#define SET_COM_PINS                0xda // 0x12 => RESET 
-#define SET_CONTRAST_CONTROL        0x81 // 0xff => MAX contrast
-#define ENTIRE_DISPLAY_ON_RESET     0xa4 // A4 resume to RAM content, A5 entire display on
-#define INVERSE_OFF                 0xa6
-#define SET_DISPLAY_CLOCK_DIVIDE    0xd5 // 0x80 => two values in one hex! Divide ratio (1) and Osc Freq. (RESET)
-#define 0x8d
-#define SET_HIGHER_COLUMN_PAGE_ADDR 0x14
-#define DISPLAY_ON                  0xaf
-#define SET_MEMORY                  0x20 // 0x02 Page addressing mode
-
-//********** SPI OPEN PORT **********
-//spi_device	0=CS0, 1=CS1
+// spi_device 0=CS0, 1=CS1
 int spi_open_port (int spi_device) {
     int *spi_cs_fd;
 
@@ -93,7 +74,8 @@ int spi_open_port (int spi_device) {
       perror("Could not set SPI speed (RD)...ioctl fail");
       exit(1);
     }
-    return 1;
+
+    return(0);
 }
 
 //********** SPI CLOSE PORT **********
@@ -107,11 +89,11 @@ int spi_close_port (int spi_device) {
     	perror("Error - Could not close SPI device");
     	exit(1);
     }
-    return 1;
+
+    return(0);
 }
 
-//********** SPI WRITE & READ DATA **********
-//data		Bytes to write.  Contents is overwritten with bytes read.
+// data	Bytes to write.  Contents is overwritten with bytes read.
 int spi_write_read (int spi_device, unsigned char *data, int length) {
 	struct spi_ioc_transfer spi[length];
     int *spi_cs_fd;
@@ -135,5 +117,7 @@ int spi_write_read (int spi_device, unsigned char *data, int length) {
 		perror("Error - Problem transmitting spi data..ioctl");
 		exit(1);
 	}
-	return 1;
+
+	return(0);
 }
+
