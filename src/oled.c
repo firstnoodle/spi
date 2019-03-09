0345decb21d7c8f7f908cbc1346bc55e3c5ccb66
sebastian thielke <seb@redboatopera.com>
2019-03-09 22:51:20 +0100

    first SPI test

--- src/oled.c
+++ src/oled.c
@@ -3,11 +3,23 @@
 int main(int argc, char *argv[]) {
 
 	printf("Hello oled\n\n");
+	/*
 	printf( "argc = %d\n", argc );
     for( int i = 0; i < argc; ++i ) {
         printf( "argv[ %d ] = %s\n", i, argv[ i ] );
     }
 	printf( "\nDISPLAY_OFF = %X\n", DISPLAY_OFF );
+	*/
+	if(spi_open_port(0)) {
+		printf("SPI port open\n");
+	}
+	// if(spi_read_write(initbuf)) {
+	// 	printf("initbuf written successfully!\n");
+	// }
+	if(spi_close_port(0)) {
+		printf("SPI port closed\n");
+	}
+	printf("Goodbye oled\n\n");
 
 	return 0;
 }
