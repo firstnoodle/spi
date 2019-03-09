747468333cf34c9ebd3f7272522154c2b4e85d8d
firstnoodle <seb@firstnoodle.io>
2019-03-09 21:45:16 +0000

    changed for-loop i declaration to be outside of for-loop (asit should)

--- src/oled.c
+++ src/oled.c
@@ -4,7 +4,8 @@ int main(int argc, char *argv[]) {
 
 	printf("Hello oled\n\n");
 	printf( "argc = %d\n", argc );
-    for( int i = 0; i < argc; ++i ) {
+	int i;
+    for(i = 0; i < argc; ++i) {
         printf( "argv[ %d ] = %s\n", i, argv[ i ] );
     }
 	printf( "\nDISPLAY_OFF = %X\n", DISPLAY_OFF );
